a82e65069d5cdf41dab55be1fd7df346958d0e7d
sigmaSd <bedisnbiba@gmail.com>
2021-03-21 16:22:45 +0000

    part2

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>[];
   final _biggerFont = TextStyle(fontSize: 18.0);
   // #enddocregion RWS-var
 
@@ -36,10 +37,15 @@ class _RandomWordsState extends State<RandomWords> {
 
   // #docregion _buildRow
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null
       ),
     );
   }
